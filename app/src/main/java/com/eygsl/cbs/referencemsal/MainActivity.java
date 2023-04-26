340b7f3700050635dea5896e872372fcb5029414
Vinayagam Krishnan <vinayagam.krishnan@ey.com>
2023-04-26 13:22:12 +0530

    app center integration

--- app/src/main/java/com/eygsl/cbs/referencemsal/MainActivity.java
+++ app/src/main/java/com/eygsl/cbs/referencemsal/MainActivity.java
@@ -4,11 +4,19 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCenter.start(getApplication(), "6d31ee02-c699-4d31-830b-a26c5b3f2309",
+                Analytics.class, Crashes.class);
+
     }
 }
